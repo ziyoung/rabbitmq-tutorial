4e424cd8783c36ff5704e92ec982460a67cce32f
hetech <dengzhiyang123@126.com>
2019-01-07 22:51:53 +0800

    add exchange

--- chapter-2/README.md
+++ chapter-2/README.md
@@ -12,3 +12,9 @@
 
 producer：声明 queue 时设置 durable 为 true。
 consumer：声明 queue 时设置 durable 为 true，消费队列时设置 auto-ack 为 false，手动确认消息是否发送成功。调用 Qos 设置 prefetch，来控制消息如何传送。
+
+#### publish-subscrible
+
+![发布订阅](https://www.rabbitmq.com/img/tutorials/exchanges.png)
+
+使用 exchange 把消息传送到两个 queue 中。producer 创建 exchange，consumer 也需要创建 exchange，创建匿名 queue，并且将 queue 与 exchange 绑定。其他与上述例子大致相同。
---
+++ chapter-2/publish-subscrible/producer/prducer.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strconv"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	var repeatTimes int
+	flag.IntVar(&repeatTimes, "repeat", 2, "重复次数")
+	flag.Parse()
+	if repeatTimes < 2 {
+		log.Fatal("repeat 次数不能小于 2")
+	}
+
+	conn, err := amqp.Dial("amqp://root:root123@localhost:8089")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	prefix := "Hello World!"
+
+	for index := 0; index < repeatTimes; index++ {
+		message := prefix + " and id is " + strconv.Itoa(index)
+		err = ch.Publish(
+			"logs", // exchange
+			"",     // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(message),
+			},
+		)
+		failOnError(err, "Failed to publish a message")
+		log.Printf("[x] Send %s", message)
+	}
+}
